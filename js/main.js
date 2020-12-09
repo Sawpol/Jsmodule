dc9220eb1de04d739781a56bc0dc3000c49a5d44
André Vázquez <sawpol123@gmail.com>
2020-12-08 19:47:46 -0600

    Add exercise weight in the moon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-
 console.log('Hola Koders')
 
-alert('Hola Koders')
+
+let name_koder= prompt('¿Cuál es tu nombre?')
+console.log(name_koder)
+let age_koder= prompt('¿Cuál es tu edad?')
+console.log(age_koder)
+
+let resultSuma=parseInt(age_koder)
+
+ console.log('suma:',resultSuma+5)
+ console.log('división:',age_koder/5)
+ console.log('multiplicación:',age_koder*5)
+ console.log('resta:',age_koder-5)
---
+++ js/weight_in_moon.js
@@ -0,0 +1,8 @@
+let peso_koder= prompt('¿Cuál es tu peso?')
+
+
+let peso_luna=(peso_koder/9.81)*1.622 
+
+alert('Tu peso es: '+ peso_luna)
+
+
